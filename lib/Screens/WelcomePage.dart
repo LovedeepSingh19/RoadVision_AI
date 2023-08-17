485dae3b62eab8c6edd3bde4ed9048849ed049b3
Lovedeep <gurugobind.sh@gmail.com>
2023-08-17 19:20:29 +0530

    verification complete

--- lib/Screens/WelcomePage.dart
+++ lib/Screens/WelcomePage.dart
@@ -1,3 +1,5 @@
+import 'package:blackcoffer_video/Screens/Homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -8,12 +10,23 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  final user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Center(
-      child: Text("hello there , Welcome"),
-    )));
+      body: Container(
+          child: Center(
+        child: Text("hello $user , Welcome"),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => HomePage()));
+        },
+        child: const Text("Logout"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
